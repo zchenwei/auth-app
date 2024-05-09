50bd52082c485f393cd10c080dc4eab16fa941d6
Chenwei Zhang <chenwz@amazon.com>
2024-05-09 13:39:48 -0700

    add user attrs

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,4 +8,58 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  userAttributes: {
+    preferredUsername: {
+      mutable: true,
+      required: false,
+    },
+    address: {
+      mutable: true,
+      required: false,
+    },
+    birthdate: {
+      mutable: true,
+      required: false,
+    },
+    gender: {
+      mutable: true,
+      required: false,
+    },
+    givenName: {
+      mutable: true,
+      required: false,
+    },
+    familyName: {
+      mutable: true,
+      required: false,
+    },
+    locale: {
+      mutable: true,
+      required: false,
+    },
+    middleName: {
+      mutable: true,
+      required: false,
+    },
+    nickname: {
+      mutable: true,
+      required: false,
+    },
+    phoneNumber: {
+      mutable: true,
+      required: false,
+    },
+    profilePicture: {
+      mutable: true,
+      required: false,
+    },
+    profilePage: {
+      mutable: true,
+      required: false,
+    },
+    website: {
+      mutable: true,
+      required: false,
+    },
+  },
 });
